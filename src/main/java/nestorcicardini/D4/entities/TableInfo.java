0fca6c9996d8911b9ed0eb5af5816636ef1d429d
Nestor162 <nestorcicardini@gmail.com>
2023-06-01 16:06:03 +0200

    Cambiato nome classe per evitare conflitti

--- src/main/java/nestorcicardini/D4/entities/TableInfo.java
+++ src/main/java/nestorcicardini/D4/entities/TableInfo.java
@@ -1,5 +1,10 @@
 package nestorcicardini.D4.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +14,11 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
-public class Table {
+@Entity
+@Table(name = "tables")
+public class TableInfo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int tableNumber;
 	private int maxCapacity;
 	private boolean occupied;
@@ -18,7 +27,7 @@ public class Table {
 		OCCUPIED, AVAILABLE
 	}
 
-	public Table(int tableNumber, int maxOccupancy, boolean status) {
+	public TableInfo(int tableNumber, int maxOccupancy, boolean status) {
 		super();
 		this.tableNumber = tableNumber;
 		this.maxCapacity = maxOccupancy;
